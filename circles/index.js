71f0acc7abbbf46b2ea3bc92f2ea92e349a4c9ab
Terrence Eve <tijayeve@gmail.com>
2019-11-05 21:07:12 -0600

    added random circle data to array for rendering using redux

--- circles/index.js
+++ circles/index.js
@@ -6,4 +6,5 @@ subscribe(() => render(getState()));
 dispatch({ type: null }); // Here we're making a call to dispatch() - this triggers the first render.
 
 // Write DOM event listeners here, make them dispatch actions to the Redux store 
-
+const button = document.getElementById("addCircle")
+button.addEventListener("click", e => dispatch({ type: "ADD_CIRCLE" }))
--- circles/reducer.js
+++ circles/reducer.js
@@ -17,11 +17,37 @@ const initialState = [
     },
 ];
 
+function copyArray(array) {
+    let newArray = [ 
+        ...array 
+    ]
+}
+
+function addToArray(array) {
+    let newArray =  [
+        ...array.push()
+    ]
+}
+
+function randomNumber(min, max) {
+    return (Math.random() * ((max - min) + 1)) + min
+}
+
 const reducer = (state = initialState, action) => {
     // Handle actions here - make sure you don't mutate the state!
-    const { type } = action;
+    let { type } = action;
 
     // ACTION: Add a random circle
+    if ( type = "ADD_CIRCLE") {
+        console.log("Hello, Buddy!")
+        return [
+            ...state,
+            {
+                radius: randomNumber(10, 100),
+                color: `rgb(${randomNumber(0, 255)}, ${randomNumber(0, 255)}, ${randomNumber(0, 255)})`
+            }
+        ]
+    }
 
     return state;
 }
